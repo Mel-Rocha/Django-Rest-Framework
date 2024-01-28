f35ce98d8f798f940ab849e793fe1a3f347a29d5
Mel-Rocha <rochamel73@gmail.com>
2024-01-27 23:25:17 -0300

    Parte 6 - usando roteadores

--- snippets/views.py
+++ snippets/views.py
@@ -4,21 +4,11 @@ from django.contrib.auth.models import User
 from snippets.serializers import UserSerializer
 from rest_framework import permissions
 from snippets.permissions import IsOwnerOrReadOnly
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 from rest_framework import renderers
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework import renderers
-
-
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
-    })
 
 
 class SnippetViewSet(viewsets.ModelViewSet):
